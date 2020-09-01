6df783e5fe49940bc8bf0bc02e875313fa90d834
Flavio Alessandri <flavio.alessandri@gmail.com>
2020-09-01 18:47:40 +0200

    Php with if/else for strpos

--- index.php
+++ index.php
@@ -2,16 +2,38 @@
 
 // Creare una variabile con un paragrafo di testo. Visualizzare a schermo il paragrafo con la relative lunghezza e sostituire la badword passata in GET con tre *.
 
-$word= $_GET["word"];
+// Get word to match with text
+$word= ucwords($_GET["word"]);
 
-$text = "ciao cavallo cane mucca asino mucca gallina pollo mucca";
-
+//Text
+$text = ucwords("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
 $textLength = strlen($text);
 
-$textReplaced = str_replace(ucwords($word), "***", ucwords($text));
 
-$textReplacedLength = strlen($textReplaced);
+echo "<h4> Text : '" .$text. " '</h4>";
+echo "<h4> Text length : " .$textLength. "</h1>" ;
 
+$wordpos = strpos($text,$word);
+
+echo $wordpos;
+
+if($wordpos>0){
+
+  // replace finded word with ***
+  $textReplaced = str_replace($word, "***",$text);
+
+  //find text replaced Length
+  $textReplacedLength = strlen($textReplaced);
+
+  echo "<h4> Modified Text : " .$textReplaced. "</h1>" ;
+  echo "<h4> Mofidied Text Length: " .$textReplacedLength. "</h1>" ;
+
+} else{
+  echo "<h4>No Word Finded for " .$word. "</h1>" ;
+};
+
+var_dump($word);
+echo "<br>";
 var_dump($text);
 echo "<br>";
 var_dump($textLength);
@@ -22,7 +44,7 @@ var_dump($textReplacedLength);
 
 ?>
 
-<h2>Testo : <?php echo $text ?></h2>
+<!-- <h4>Testo : <?php echo $text ?></h4>
 <h3>Lunghezza testo : " <?php echo $textLength ?> "</h3>
-<h2>Testo Modificato : " <?php echo $textReplaced ?> "</h2>
-<h3>Lunghezza testo Modificato : " <?php echo $textReplacedLength ?> "</h3>
+<h4>Testo Modificato : " <?php echo $textReplaced ?> "</h4>
+<h3>Lunghezza testo Modificato : " <?php echo $textReplacedLength ?> "</h3> -->
